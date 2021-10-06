ceaa425f5567f07acf5c71c8b83392030f4b234e
Aleksey Gusev <audetv@gmail.com>
2021-10-06 10:33:42 +0300

    Ran server use the http.Server and defines params for running

--- lesson4/main.go
+++ lesson4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type helloHandler struct {
@@ -20,7 +21,13 @@ func main() {
 	http.Handle("/world", worldHandler)
 	http.Handle("/room", roomHandler)
 
-	err := http.ListenAndServe(":80", nil)
+	srv := &http.Server{
+		Addr:         "localhost:80",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	err := srv.ListenAndServe()
 	if err != nil {
 		return
 	}
