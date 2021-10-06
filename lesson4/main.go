235fbf95cd4a7db27fd4ca3d0d817f50c53fceb4
Aleksey Gusev <audetv@gmail.com>
2021-10-06 10:48:14 +0300

    Added example of handle request method

--- lesson4/main.go
+++ lesson4/main.go
@@ -11,15 +11,21 @@ type helloHandler struct {
 }
 
 func (h *helloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s!", h.subject)
+	// fmt.Fprintf(w, "Hello, %s!", h.subject)
+	switch r.Method {
+	case http.MethodGet:
+		fmt.Fprintln(w, "some GET-related logic")
+	case http.MethodPost:
+		fmt.Fprintln(w, "some POST-related logic")
+	}
 }
 
 func main() {
-	worldHandler := &helloHandler{"World"}
-	roomHandler := &helloHandler{"Mark"}
+	worldHandler := helloHandler{"World"}
+	roomHandler := helloHandler{"Mark"}
 
-	http.Handle("/world", worldHandler)
-	http.Handle("/room", roomHandler)
+	http.Handle("/world", &worldHandler)
+	http.Handle("/room", &roomHandler)
 
 	srv := &http.Server{
 		Addr:         "localhost:80",
