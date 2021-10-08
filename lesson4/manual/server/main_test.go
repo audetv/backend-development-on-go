e8bc22f200a580bfa5096c93c327b3eb1c434ae8
Aleksey Gusev <audetv@gmail.com>
2021-10-08 12:09:31 +0300

    Added test for upload handler

--- lesson4/manual/server/main_test.go
+++ lesson4/manual/server/main_test.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"log"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -32,6 +40,58 @@ func TestHandler_ServeHTTP(t *testing.T) {
 	// Проверяем тело ответа
 	expected := `Parsed query-param with key "name": Jhon`
 	if rr.Body.String() != expected {
+		t.Errorf("Handler returned unexpected body: got %v expect %v", rr.Body.String(), expected)
+	}
+}
+
+func TestUploadHandler_ServeHTTP(t *testing.T) {
+	// открываем файл, который хотим отправить
+	file, err := os.Open("testfile")
+	if err != nil {
+		log.Println(err)
+	}
+	defer file.Close()
+
+	// действия, необходимые для того, чтобы засунуть файл в запрос
+	// в качестве мультипарт-формы
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, _ := writer.CreateFormFile("file", filepath.Base(file.Name()))
+	io.Copy(part, file)
+	writer.Close()
+
+	// опять создаем запрос, теперь уже на /upload эндпоинт
+	req, _ := http.NewRequest(http.MethodPost, "/upload", body)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+
+	// создаем ResponseRecorder
+	rr := httptest.NewRecorder()
+
+	// создаем заглушку файлового сервера. Для прохождения тестов
+	// нам достаточно чтобы он возвращал 200 статус
+	ts := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprintln(writer, "ok!")
+	}))
+	defer ts.Close()
+
+	uploadHandler := &UploadHandler{
+		UploadDir: "upload",
+		// таким образом мы подменим адрес файлового сервера
+		// и вместо реального, хэндлер будет стучаться на заглушку
+		// которая всегда будет возвращать 200 статус, что нам и нужно
+		HostAddr: ts.URL,
+	}
+
+	// опять же, вызываем ServeHTTP у тестируемого обработчика
+	uploadHandler.ServeHTTP(rr, req)
+
+	// Проверяем статус-код ответа
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v expext %v", status, http.StatusOK)
+	}
+
+	expected := "testfile"
+	if !strings.Contains(rr.Body.String(), expected) {
 		t.Errorf("Handler returned unexpected body: got %v expect %v", rr.Body.String(), expected)
 	}
 }
