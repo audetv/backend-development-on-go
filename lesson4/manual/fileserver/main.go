6ef5675312881fee522faa596c93646087071928
Aleksey Gusev <audetv@gmail.com>
2021-10-08 17:36:18 +0300

    Changed response type to json, added flusher

--- lesson4/manual/fileserver/main.go
+++ lesson4/manual/fileserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -13,23 +14,53 @@ type FilesHandler struct {
 	UploadDir string
 }
 
+type File struct {
+	Name string `json:"name"`
+	Ext  string `json:"ext"`
+	Size int64  `json:"size"`
+}
+
 func (f *FilesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	files, err := ioutil.ReadDir(f.UploadDir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	w.Header().Set("Content-Type", "application/json")
+
+	enc := json.NewEncoder(w)
+
+	comma := true
+	fmt.Fprintf(w, "[")
+	defer fmt.Fprintln(w, "]")
+
 	for _, file := range files {
-		extension := filepath.Ext(file.Name())
-		fmt.Fprintf(w, "name: %v, ext: %v, size: %v\r\n", file.Name(), extension, file.Size())
+		f := &File{
+			Name: file.Name(),
+			Ext:  filepath.Ext(file.Name()),
+			Size: file.Size(),
+		}
+		if comma {
+			comma = false
+		} else {
+			fmt.Fprintf(w, ",")
+		}
+		_ = enc.Encode(f)
+		w.(http.Flusher).Flush()
 	}
+
+	// for _, file := range files {
+	// 	extension := filepath.Ext(file.Name())
+	// 	fmt.Fprintf(w, "name: %v, ext: %v, size: %v\r\n", file.Name(), extension, file.Size())
+	// }
+
 }
 
 func main() {
 	dirToServe := http.Dir("./upload")
 
 	filesHandler := &FilesHandler{
-		UploadDir: "upload",
+		UploadDir: string(dirToServe),
 	}
 
 	fs := &http.Server{
