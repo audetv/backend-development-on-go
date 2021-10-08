c1259d44078ab8e79a6c7bf854f5fd199584ddb7
Aleksey Gusev <audetv@gmail.com>
2021-10-08 15:20:17 +0300

    Added files handler,response files list

--- lesson4/manual/fileserver/main.go
+++ lesson4/manual/fileserver/main.go
@@ -1,20 +1,46 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
 	"time"
 )
+
+type FilesHandler struct {
+	UploadDir string
+}
+
+func (f *FilesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	files, err := ioutil.ReadDir(f.UploadDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		extension := filepath.Ext(file.Name())
+		fmt.Fprintf(w, "name: %v, ext: %v, size: %v\r\n", file.Name(), extension, file.Size())
+	}
+}
 
 func main() {
 	dirToServe := http.Dir("./upload")
 
+	filesHandler := &FilesHandler{
+		UploadDir: "upload",
+	}
+
 	fs := &http.Server{
 		Addr:         "localhost:3002",
-		Handler:      http.FileServer(dirToServe),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
+
+	http.Handle("/", http.FileServer(dirToServe))
+	http.Handle("/files", filesHandler)
+
 	err := fs.ListenAndServe()
 	if err != nil {
 		log.Println(err)
