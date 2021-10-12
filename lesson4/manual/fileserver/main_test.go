ddf6dfa4ebfcb4c62019b7da01ecdafcd4ce812e
Aleksey Gusev <audetv@gmail.com>
2021-10-12 11:59:55 +0300

    Redesigned the tests

--- lesson4/manual/fileserver/main_test.go
+++ lesson4/manual/fileserver/main_test.go
@@ -7,71 +7,57 @@ import (
 	"testing"
 )
 
-func TestFileServer(t *testing.T) {
-	req, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
+func TestHandler_ServeHTTP(t *testing.T) {
+	cases := []struct {
+		dirToServe string
+		url        string
+		expected   string
+		returnErr  bool
+		name       string
+	}{
+		{
+			dirToServe: "testdata/upload",
+			url:        "/files",
+			expected:   `[{"name":"34cf8110-d6f7-4c72-8126-e2b33917f6ae.testfile.txt","ext":".txt","size":36},{"name":"3ad59372-cc02-4b0a-8e80-1f7a0e6a541e.testfile1.txt","ext":".txt","size":0},{"name":"8848ae1f-a31c-42b8-b512-4ec57906f065.testfile2","ext":".testfile2","size":0}]`,
+			name:       "ListOfFiles",
+		},
+		{
+			dirToServe: "testdata/upload",
+			url:        "/files?ext=jpg",
+			expected:   "[]",
+			returnErr:  false,
+			name:       "NoMatchesFound",
+		},
+		{
+			dirToServe: "testdata/upload",
+			url:        "/files?ext=txt",
+			expected:   `[{"name":"34cf8110-d6f7-4c72-8126-e2b33917f6ae.testfile.txt","ext":".txt","size":36},{"name":"3ad59372-cc02-4b0a-8e80-1f7a0e6a541e.testfile1.txt","ext":".txt","size":0}]`,
+			returnErr:  false,
+			name:       "MatchesFound",
+		},
 	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodGet, tc.url, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	rr := httptest.NewRecorder()
-	dirToServe := http.Dir("upload")
-	handler := http.FileServer(dirToServe)
-	handler.ServeHTTP(rr, req)
+			rr := httptest.NewRecorder()
+			dirToServe := http.Dir(tc.dirToServe)
+			handler := FilesHandler{
+				UploadDir: string(dirToServe),
+			}
+			handler.ServeHTTP(rr, req)
 
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Handler returned wrong status code: got %v expect %v", status, http.StatusOK)
-	}
+			if status := rr.Code; status != http.StatusOK {
+				t.Errorf("Handler returned wrong status code: got %v expect %v", status, http.StatusOK)
+			}
 
-	expected := `<pre><a href="34cf8110-d6f7-4c72-8126-e2b33917f6ae.testfile.txt">34cf8110-d6f7-4c72-8126-e2b33917f6ae.testfile.txt</a><a href="3ad59372-cc02-4b0a-8e80-1f7a0e6a541e.testfile1.txt">3ad59372-cc02-4b0a-8e80-1f7a0e6a541e.testfile1.txt</a></pre>`
-	if strings.ReplaceAll(rr.Body.String(), "\n", "") != expected {
-		t.Errorf("Handler returned unexpected body: got %v expect %v", rr.Body, expected)
-	}
-}
-
-// Тестируем фильтр ext=jpg, который должен вернуть пустой массив
-func TestFilesHandler_ExtEmptyResponse(t *testing.T) {
-	req, err := http.NewRequest(http.MethodGet, "/files?ext=jpg", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	dirToServe := http.Dir("upload")
-	handler := FilesHandler{
-		UploadDir: string(dirToServe),
-	}
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Handler returned wrong status code: got %v expect %v", status, http.StatusOK)
-	}
-
-	expected := "[]"
-	if strings.ReplaceAll(rr.Body.String(), "\n", "") != expected {
-		t.Errorf("Handler returned unexpected body: got %v expect %v", rr.Body, expected)
-	}
-}
-
-// Тестируем фильтр файлов ext=txt который должен вернуть массив json из 2-х объектов
-func TestFilesHandler_ExtWithJsonResponse(t *testing.T) {
-	req, err := http.NewRequest(http.MethodGet, "/files?ext=txt", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	rr := httptest.NewRecorder()
-	dirToServe := http.Dir("upload")
-	handler := FilesHandler{
-		UploadDir: string(dirToServe),
-	}
-	handler.ServeHTTP(rr, req)
-
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("Handler returned wrong status code: got %v expect %v", status, http.StatusOK)
-	}
-
-	expected := `[{"name":"34cf8110-d6f7-4c72-8126-e2b33917f6ae.testfile.txt","ext":".txt","size":36},{"name":"3ad59372-cc02-4b0a-8e80-1f7a0e6a541e.testfile1.txt","ext":".txt","size":0}]`
-	if strings.ReplaceAll(rr.Body.String(), "\n", "") != expected {
-		t.Errorf("Handler returned unexpected body: got %v expect %v", rr.Body, expected)
+			expected := tc.expected
+			if strings.ReplaceAll(rr.Body.String(), "\n", "") != expected {
+				t.Errorf("Handler returned unexpected body: got %v expect %v", rr.Body, expected)
+			}
+		})
 	}
 }
